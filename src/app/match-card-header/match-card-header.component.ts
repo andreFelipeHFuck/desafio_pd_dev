48d44b1c537f99612f8de0ecd09aa1e1def22b24
Andre Felipe <andrefelipehf2001@gmail.com>
2025-09-09 06:48:22 -0300

    Componente match-card-header, lógica da data e do dia da semana

--- src/app/match-card-header/match-card-header.component.ts
+++ src/app/match-card-header/match-card-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-match-card-header',
@@ -7,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './match-card-header.component.scss'
 })
 export class MatchCardHeaderComponent {
+  date = input.required<string>();
 
+
+  private formatMount(date: Date): string{
+    const mount = date
+                  .toLocaleDateString('pt-BR', {month: 'short'})
+                  .split('.')[0];
+
+    return mount.charAt(0).toUpperCase() + mount.slice(1);
+  }
+
+  formatDateShort(dateString: string): string{
+    if (!dateString) return '';
+    
+    const date: Date = new Date(dateString);
+
+    const day = date.getDay().toString().padStart(2, '0');
+    const mount = this.formatMount(date);
+    const year = date.getFullYear();
+
+    return `${day}/${mount}/${year}`;
+  }
+
+  formatWeekday(dateString: string): string{
+     if (!dateString) return '';
+
+    const date: Date = new Date(dateString);
+    const weeekday = date.toLocaleDateString('pt-BR', {weekday: 'long'});
+
+    return weeekday.charAt(0).toUpperCase() + weeekday.slice(1);
+  }
 }
